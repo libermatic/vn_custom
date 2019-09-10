93ab2f5a954e09ae1c956f61a490206b4c44654c
Sun Howwrongbum <sun@libermatic.com>
2019-09-10 13:38:23 +0530

    feat(Item Price): update list view

--- vn_custom/hooks.py
+++ vn_custom/hooks.py
@@ -28,6 +28,9 @@ fixtures = [
                     "Supplier-supplier_name-in_standard_filter",
                     "Item-naming_series-options",
                     "Item-item_name-in_standard_filter",
+                    "Item Price-min_qty-in_standard_filter",
+                    "Item Price-min_qty-in_list_view",
+                    "Item Price-price_list-in_list_view",
                     "Sales Invoice-naming_series-options",
                 ],
             ]
