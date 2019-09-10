4f2e4a2e8f382e030fd7fc33b12a5edd1a1a46f2
Sun Howwrongbum <sun@libermatic.com>
2019-09-10 13:39:26 +0530

    feat(VN Settings): create this

--- vn_custom/install.py
+++ vn_custom/install.py
@@ -2,11 +2,24 @@
 from __future__ import unicode_literals
 import frappe
 from frappe import _
+from toolz import merge
+
+documents = {
+    "Price List": [
+        {
+            "price_list_name": "MRP",
+            "currency": frappe.defaults.get_global_default("currency"),
+            "enabled": 1,
+            "selling": 1,
+        }
+    ]
+}
 
 
 settings = {
     "Selling Settings": {"cust_master_name": "Naming Series"},
     "Stock Settings": {"item_naming_by": "Naming Series", "show_barcode_field": 1},
+    "VN Settings": {"mrp_price_list": "MRP"},
 }
 
 
@@ -14,6 +27,7 @@ settings = {
 def setup_defaults():
     if frappe.session.user != "Administrator":
         frappe.throw(_("Only allowed for Administrator"))
+    _create_docs()
     _update_settings()
 
 
@@ -25,3 +39,15 @@ def _update_settings():
 
     for x in settings.items():
         update(*x)
+
+
+def _create_docs():
+    def insert(doctype, args):
+        if not frappe.db.exists(doctype, args):
+            frappe.get_doc(merge({"doctype": doctype}, args)).insert(
+                ignore_permissions=True
+            )
+
+    for doctype, docs in documents.items():
+        for doc in docs:
+            insert(doctype, doc)
