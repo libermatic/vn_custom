b21e0b7d7d3826a61b9537d577f37baeeafc2e8e
Sun Howwrongbum <sun@libermatic.com>
2019-09-18 21:11:05 +0530

    feat(Wire Transfer Settings): add bank_accounts

--- vn_custom/wire_transfer/doctype/wire_transfer_settings/wire_transfer_settings.js
+++ vn_custom/wire_transfer/doctype/wire_transfer_settings/wire_transfer_settings.js
@@ -4,6 +4,10 @@
 frappe.ui.form.on('Wire Transfer Settings', {
   setup: function(frm) {
     frm.set_query('cash_account', { account_type: 'Cash', is_group: 0 });
+    frm.set_query('bank_account', 'bank_accounts', {
+      account_type: 'Bank',
+      is_group: 0,
+    });
     frm.set_query('transit_account', { root_type: 'Liability', is_group: 0 });
   },
 });
