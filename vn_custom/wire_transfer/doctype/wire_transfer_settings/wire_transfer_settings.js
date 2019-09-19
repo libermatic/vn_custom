94c380c711412e601c425e9f4e88d0f93e72b32a
Sun Howwrongbum <sun@libermatic.com>
2019-09-20 01:31:41 +0530

    fix(Wire Transfer Settings): add more default fields

--- vn_custom/wire_transfer/doctype/wire_transfer_settings/wire_transfer_settings.js
+++ vn_custom/wire_transfer/doctype/wire_transfer_settings/wire_transfer_settings.js
@@ -6,5 +6,10 @@ frappe.ui.form.on('Wire Transfer Settings', {
     frm.set_query('cash_account', { account_type: 'Cash', is_group: 0 });
     frm.set_query('bank_account', { account_type: 'Bank', is_group: 0 });
     frm.set_query('transit_account', { root_type: 'Liability', is_group: 0 });
+    frm.set_query('income_account', {
+      account_type: 'Income Account',
+      is_group: 0,
+    });
+    frm.set_query('cost_center', { is_group: 0 });
   },
 });
