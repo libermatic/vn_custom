1f2562a03af37269d6b9ee3e956e55b69c49a94a
Sun Howwrongbum <sun@libermatic.com>
2023-03-24 19:13:54 +0000

    fix: `sales_uom` not getting applied

--- vn_custom/public/js/custom_scripts/sales_invoice.js
+++ vn_custom/public/js/custom_scripts/sales_invoice.js
@@ -1,16 +1,29 @@
-async function set_other_prices(frm, cdt, cdn) {
-  const { item_code, uom } = frappe.get_doc(cdt, cdn) || {};
-  if (item_code) {
-    const {
-      message: { vn_mrp = 0, vn_valuation = 0 } = {},
-    } = await frappe.call({
-      method: 'vn_custom.api.item.get_other_prices',
-      args: { item_code, uom },
-    });
-    frappe.model.set_value(cdt, cdn, { vn_mrp, vn_valuation });
-  } else {
-    frappe.model.set_value(cdt, cdn, { vn_mrp: null, vn_valuation: null });
-  }
+const sales_invoice_item = {
+  item_code: async function (frm, cdt, cdn) {
+    const { item_code } = frappe.get_doc(cdt, cdn);
+    if (item_code) {
+      const {
+        message: { stock_uom, sales_uom },
+      } = await frappe.db.get_value('Item', item_code, [
+        'stock_uom',
+        'sales_uom',
+      ]);
+      await frappe.model.set_value(cdt, cdn, { uom: stock_uom || sales_uom });
+    }
+  },
+
+  uom: async function (frm, cdt, cdn) {
+    const { item_code, uom } = frappe.get_doc(cdt, cdn) || {};
+    if (item_code) {
+      const { message: { vn_mrp = 0, vn_valuation = 0 } = {} } =
+        await frappe.call({
+          method: 'vn_custom.api.item.get_other_prices',
+          args: { item_code, uom },
+        });
+      frappe.model.set_value(cdt, cdn, { vn_mrp, vn_valuation });
+    } else {
+      frappe.model.set_value(cdt, cdn, { vn_mrp: null, vn_valuation: null });
+    }
   },
 };
 
@@ -24,7 +37,7 @@ const sales_invoice = {
           query: 'vn_custom.api.item.get_uom',
           filters: { item_code },
         };
-}
+      }
     });
   },
 };
